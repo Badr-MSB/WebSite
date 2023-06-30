1118f45db0f9be88f3a0435471ce6f5ed04c6953
Badr <badrmesbahi29@gmail.com>
2023-06-30 13:14:06 +0200

    second

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,18 +3,34 @@ import { Carousel } from 'react-responsive-carousel';
 
 import React from 'react'
 
+import './Carousel.css'
+
 const Carousell = ({ images }) => {
+    const onClickItemHandler = (index) => {
+        console.log(index)
+    }
     return (
-        <Carousel showThumbs={false} showStatus={false} showArrows={true} autoPlay={true} infiniteLoop={true}>
-            {images.map((image, index) => (
-                <div>
-                    <img
-                        key={index}
-                        src={image}
-                    />
-                </div>
-            ))}
-        </Carousel>
+        <>
+            <Carousel
+                showThumbs={false}
+                showStatus={false}
+                showArrows={true}
+                autoPlay={true}
+                infiniteLoop={true}
+                className="crousel"
+                onClickItem={(index) => onClickItemHandler(index)}
+            >
+                {images.map((image, index) => (
+                    <div>
+                        <img
+                            key={`image_${index + 1}`}
+                            src={image}
+                            className=""
+                        />
+                    </div>
+                ))}
+            </Carousel>
+        </>
     )
 }
 
